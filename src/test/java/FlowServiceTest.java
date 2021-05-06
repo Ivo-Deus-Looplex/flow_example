fd39073c03e1fb7c8bd56268b9387abba8f8c9dd
Ivo de Andrade de Deus <ivo.deus@looplex.com.br>
2021-05-06 16:09:46 -0300

    Fixed test class setup, moved FlowApplication

--- src/test/java/FlowServiceTest.java
+++ src/test/java/FlowServiceTest.java
@@ -3,6 +3,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import workflow.FlowApplication;
 import workflow.flows.SimpleFlow;
 import workflow.stages.UploadTemplate;
 
